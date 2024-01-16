0237dd35af23714ddc7cbc307bb746313bcf1a81
Tấn <tannvgdh210106@fpt.edu.vn>
2024-01-16 13:42:25 +0700

    Header

--- shoping-store/src/styles/header.js
+++ shoping-store/src/styles/header.js
@@ -1,33 +1,52 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { FaCartShopping } from "react-icons/fa6";
 
 export const HeaderContainer = styled.div`
-  background-color: #333;
-  color: white;
-  padding: 10px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+  background-color: white;
+  color: black;
 `;
 
-export const Nav = styled.nav`
+export const HeaderWrapper = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px;
+  gap: 1rem;
+`;
+
+export const Nav = styled.div`
+  display: flex;
+  gap: 1rem;
+  margin-left: 3rem;
+  font-size: 17px;
+  font-family: sans-serif;
 `;
 
 export const NavLink = styled(Link)`
-  color: white;
+  color: black;
   text-decoration: none;
   margin-right: 20px;
 `;
 
-export const Title = styled.h1`
+export const Title = styled.h2`
   margin: 0;
+  color: #3b82f6;
+  width: 12rem;
 `;
 
 export const CartLink = styled(Link)`
-  color: white;
+  color: black;
   text-decoration: none;
   display: flex;
   align-items: center;
-  margin-right: 20px;
+  justify-content: flex-end;
+  margin-right: 3rem;
+  gap: 7rem;
+`;
+
+export const StyledCartIcon = styled(FaCartShopping)`
+  color: #3b82f6;
+  font-size: 1.5rem;
+  margin-right: 5px;
 `;
