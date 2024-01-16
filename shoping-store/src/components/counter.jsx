09cae1b02748942ee54d09960da509ad7bd34ef8
Minh Khang <khangbmgch210522@fpt.edu.vn>
2024-01-16 09:49:45 +0700

    project structure

---
+++ shoping-store/src/components/counter.jsx
@@ -0,0 +1,22 @@
+// src/components/Counter.js
+import React from 'react';
+import { connect } from 'react-redux';
+import { increment, decrement } from '../redux/actions/counterActions';
+
+const Counter = ({ count, increment, decrement }) => {
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    count: state.counter, // Assuming you have a 'counter' reducer in your rootReducer
+  };
+};
+
+export default connect(mapStateToProps, { increment, decrement })(Counter);
