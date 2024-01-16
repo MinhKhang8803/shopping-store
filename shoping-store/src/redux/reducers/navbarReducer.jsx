d5eb4c8919497def2a7c863c879fabd77b641f70
Minh Khang <khangbmgch210522@fpt.edu.vn>
2024-01-16 10:19:48 +0700

    Update navbar ver 1

---
+++ shoping-store/src/redux/reducers/navbarReducer.jsx
@@ -0,0 +1,17 @@
+const initialState = {
+    isOpen: false,
+  };
+  
+  const navbarReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case 'TOGGLE_NAVBAR':
+        return {
+          ...state,
+          isOpen: !state.isOpen,
+        };
+      default:
+        return state;
+    }
+  };
+  
+  export default navbarReducer;
